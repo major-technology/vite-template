f3880aef01310459e7aa16c30a1bf4d6efb0f72e
Jason Bao <jason@glencoco.io>
2025-11-21 14:29:31 -0800

    Simple landing page (#2)

--- src/App.tsx
+++ src/App.tsx
@@ -2,10 +2,23 @@ import './App.css'
 
 function App() {
   return (
-    <>
-      <div>
-      </div>
-    </>
+    <div className="flex min-h-screen flex-col items-center justify-center bg-white p-4 text-center font-sans">
+      <h1 className="mb-4 text-4xl font-extrabold tracking-tight text-gray-900 sm:text-5xl">
+        Welcome to Major!
+      </h1>
+      <p className="text-lg text-gray-600">
+        Visit{' '}
+        <a 
+          href="https://docs.major.build" 
+          className="font-medium text-blue-600 hover:text-blue-500 hover:underline"
+          target="_blank"
+          rel="noreferrer"
+        >
+          https://docs.major.build
+        </a>{' '}
+        to view our documentation and start building.
+      </p>
+    </div>
   )
 }
 
