bf913fa2f3872a8f50b85511aca841323b584c23
josegironn <30703536+josegironn@users.noreply.github.com>
2025-12-18 18:07:14 -0800

    Upgrade packages and fix alias issue

--- vite.config.ts
+++ vite.config.ts
@@ -1,3 +1,4 @@
+import path from 'path'
 import { defineConfig, loadEnv } from 'vite'
 import type { ViteDevServer } from 'vite'
 import react from '@vitejs/plugin-react-swc'
@@ -16,6 +17,11 @@ export default defineConfig(({ mode }) => {
   const apiTarget = env.MAJOR_API_BASE_URL || 'https://api.prod.major.build'
   
   return {
+    resolve: {
+      alias: {
+        '@': path.resolve(__dirname, './src'),
+      },
+    },
     plugins: [
       react(),
       {
